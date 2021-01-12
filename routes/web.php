5270f5117280534b859e93472832a51a183c37f6
feridekrpnr <feride.karpinar8@gmail.com>
2021-01-12 04:22:00 +0300

    Add Category oluşturuldu

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,8 @@ Route::middleware('auth')->prefix('admin')->group(function(){
 
     Route::get('category',[\App\Http\Controllers\Admin\CategoryController::class,'index'])->name('admin_category');
     Route::get('category/add',[\App\Http\Controllers\Admin\CategoryController::class,'add'])->name('admin_category_add');
-    Route::get('category/update',[\App\Http\Controllers\Admin\CategoryController::class,'update'])->name('admin_category_update');
+    Route::post('category/create',[\App\Http\Controllers\Admin\CategoryController::class,'create'])->name('admin_category_create');
+    Route::post('category/update',[\App\Http\Controllers\Admin\CategoryController::class,'update'])->name('admin_category_update');
     Route::get('category/delete',[\App\Http\Controllers\Admin\CategoryController::class,'delete'])->name('admin_category_delete');
     Route::get('category/show',[\App\Http\Controllers\Admin\CategoryController::class,'show'])->name('admin_category_show');
 
