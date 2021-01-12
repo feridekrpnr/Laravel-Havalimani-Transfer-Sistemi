4093a6d2b5b8377f7d0d3e8cabb31a1557d11881
feridekrpnr <feride.karpinar8@gmail.com>
2021-01-13 00:38:31 +0300

    Kategori ekleme ve silme işlemleri yapıldı.

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::middleware('auth')->prefix('admin')->group(function(){
     Route::get('category/add',[\App\Http\Controllers\Admin\CategoryController::class,'add'])->name('admin_category_add');
     Route::post('category/create',[\App\Http\Controllers\Admin\CategoryController::class,'create'])->name('admin_category_create');
     Route::post('category/update',[\App\Http\Controllers\Admin\CategoryController::class,'update'])->name('admin_category_update');
-    Route::get('category/delete',[\App\Http\Controllers\Admin\CategoryController::class,'delete'])->name('admin_category_delete');
+    Route::get('category/delete/{id}',[\App\Http\Controllers\Admin\CategoryController::class,'destroy'])->name('admin_category_delete');
     Route::get('category/show',[\App\Http\Controllers\Admin\CategoryController::class,'show'])->name('admin_category_show');
 
 
