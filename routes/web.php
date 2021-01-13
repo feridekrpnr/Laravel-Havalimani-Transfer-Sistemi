4834a736e787d537133eaaa89866c454360b1f96
feridekrpnr <feride.karpinar8@gmail.com>
2021-01-13 03:09:17 +0300

    Kategori düzenleme formu yapıldı.

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,8 @@ Route::middleware('auth')->prefix('admin')->group(function(){
     Route::get('category',[\App\Http\Controllers\Admin\CategoryController::class,'index'])->name('admin_category');
     Route::get('category/add',[\App\Http\Controllers\Admin\CategoryController::class,'add'])->name('admin_category_add');
     Route::post('category/create',[\App\Http\Controllers\Admin\CategoryController::class,'create'])->name('admin_category_create');
-    Route::post('category/update',[\App\Http\Controllers\Admin\CategoryController::class,'update'])->name('admin_category_update');
+    Route::get('category/edit/{id}',[\App\Http\Controllers\Admin\CategoryController::class,'edit'])->name('admin_category_edit');
+    Route::post('category/update/{id}',[\App\Http\Controllers\Admin\CategoryController::class,'update'])->name('admin_category_update');
     Route::get('category/delete/{id}',[\App\Http\Controllers\Admin\CategoryController::class,'destroy'])->name('admin_category_delete');
     Route::get('category/show',[\App\Http\Controllers\Admin\CategoryController::class,'show'])->name('admin_category_show');
 
